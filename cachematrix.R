44f851f1e76d9b4ba4dd87635aaf4854186621ab
nprater1203 <122847718+nprater1203@users.noreply.github.com>
2023-10-06 14:13:21 -0700

    This commit solves the Programming Assingment 2 by creating a special "matrix" and returning its inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# This function leverages the makeVector function written in the assingment
+# description, but is modified to create a special "matrix" 
+# object that can cache the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  
+  list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+# This function leverages the cachemean function in the description
+# of the assignment. It finds the inverse of the special "matrix" that was
+# retunred from the makeCahceMatrix. If the matrix has already been solved
+# the it should just get that value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m
 }
